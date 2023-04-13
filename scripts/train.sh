2c4900683dcf79d7045a2804fad9d252322f94a5
alisonkim <alison.y.kim@berkeley.edu>
2023-04-13 10:52:49 -0700

    Update training scripts

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,8 +6,10 @@ base=$(realpath $scripts/..)
 models=$base/models
 data=$base/data
 tools=$base/tools
+logs=$base/logs
 
 mkdir -p $models
+mkdir -p $logs
 
 num_threads=4
 device=""
@@ -15,11 +17,15 @@ device=""
 SECONDS=0
 
 (cd $tools/pytorch-examples/word_language_model &&
-    CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python main.py --data $data/grimm \
-        --epochs 40 \
+    CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python main.py \
+        --data $data/tarantino \
+        --epochs 50 \
+        --batch_size 20 \
+        --mps \
         --log-interval 100 \
         --emsize 200 --nhid 200 --dropout 0.5 --tied \
-        --save $models/model.pt
+        --save_model $models/model_1.pt \
+        --log-print-statements $logs/log_1.log
 )
 
 echo "time taken:"
