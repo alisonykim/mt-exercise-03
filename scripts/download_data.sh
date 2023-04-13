37f4df9a5e2853737044b1b166254bdccb320df3
alisonkim <alison.y.kim@berkeley.edu>
2023-04-13 01:29:12 -0700

    Modify to download, preprocess, and split screenplays

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -20,24 +20,45 @@ done
 
 # download a different interesting data set!
 
-mkdir -p $data/grimm
+echo 'Downloading raw screenplays'
+mkdir -p $data/tarantino
+mkdir -p $data/tarantino/raw
+curl -o $data/tarantino/raw/django.txt https://imsdb.com/scripts/Django-Unchained.html
+curl -o $data/tarantino/raw/inglorious_basterds.txt https://imsdb.com/scripts/Inglourious-Basterds.html
+curl -o $data/tarantino/raw/jackie_brown.txt https://www.dailyscript.com/scripts/jackiebrown.html
+curl -o $data/tarantino/raw/kill_bill.txt https://imsdb.com/scripts/Kill-Bill-Volume-1-%2526-2.html
+curl -o $data/tarantino/raw/pulp_fiction.txt https://www.dailyscript.com/scripts/pulp_fiction.html
+curl -o $data/tarantino/raw/reservoir_dogs.txt https://www.dailyscript.com/scripts/Reservoir+Dogs.txt
 
-mkdir -p $data/grimm/raw
+# remove beginning credits from both screenplays and combine into single screenplay file
 
-wget https://www.gutenberg.org/files/52521/52521-0.txt
-mv 52521-0.txt $data/grimm/raw/tales.txt
+echo 'Concatenate screenplays into single file'
+{ cat $data/tarantino/raw/pulp_fiction.txt ; \
+tail -n +330 $data/tarantino/raw/django.txt ; \
+tail -n +222 $data/tarantino/raw/inglorious_basterds.txt ; \
+tail -n +49 $data/tarantino/raw/jackie_brown.txt ; \
+tail -n +219 $data/tarantino/raw/kill_bill.txt ; \
+tail -n +21 $data/tarantino/raw/reservoir_dogs.txt ; } > \
+$data/tarantino/raw/tarantino.txt
 
 # preprocess slightly
 
-cat $data/grimm/raw/tales.txt | python $base/scripts/preprocess_raw.py > $data/grimm/raw/tales.cleaned.txt
+echo 'Preprocess'
+cat $data/tarantino/raw/tarantino.txt | python $base/scripts/preprocess_raw.py > $data/tarantino/raw/tarantino.cleaned.txt
 
 # tokenize, fix vocabulary upper bound
 
-cat $data/grimm/raw/tales.cleaned.txt | python $base/scripts/preprocess.py --vocab-size 5000 --tokenize --lang "en" --sent-tokenize > \
-    $data/grimm/raw/tales.preprocessed.txt
+echo 'Tokenize, fix vocabulary upper bound'
+cat $data/tarantino/raw/tarantino.cleaned.txt | python $base/scripts/preprocess.py --vocab-size 5000 --tokenize --lang 'en' --sent-tokenize --language 'english' > \
+    $data/tarantino/raw/tarantino.preprocessed.txt
+
 
 # split into train, valid and test
 
-head -n 440 $data/grimm/raw/tales.preprocessed.txt | tail -n 400 > $data/grimm/valid.txt
-head -n 840 $data/grimm/raw/tales.preprocessed.txt | tail -n 400 > $data/grimm/test.txt
-tail -n 3075 $data/grimm/raw/tales.preprocessed.txt | head -n 2955 > $data/grimm/train.txt
+echo 'Shuffle lines'
+shuf data/tarantino/raw/tarantino.preprocessed.txt >> data/tarantino/raw/tarantino.shuffled.txt
+
+echo 'Split train/valid/test'
+head -n 14400 $data/tarantino/raw/tarantino.shuffled.txt > $data/tarantino/train.txt
+tail -n +14401 $data/tarantino/raw/tarantino.shuffled.txt | head -n 1800 > $data/tarantino/valid.txt
+tail -n +16200 $data/tarantino/raw/tarantino.shuffled.txt > $data/tarantino/test.txt
--- scripts/preprocess_raw.py
+++ scripts/preprocess_raw.py
@@ -1,9 +1,16 @@
 import sys
+import re
 
-for line in sys.stdin:
-    if line.strip() == "":
-        continue
-    else:
-        line = line.replace(u'\ufeff', '')
-        line = " ".join(line.split())
-        sys.stdout.write(line + "\n")
+
+with open(0, 'rb') as f_in:
+	for line in f_in:
+		line = line.decode('utf-8', errors='ignore').strip()
+		html_tags = r'\<.+\>'
+		parentheticals = r'\(.+\)'
+		css_tags = r'\#+.+'
+		digits = r'\d+'
+		salutations = r'(The|Dr|Mr|Mrs|Ms)\.+([A-Z]){2,}'
+		remove = '|'.join([html_tags, parentheticals, css_tags, digits, salutations])
+		line = re.sub(remove, '', line)
+		if line.upper() != line and len(line.split()) > 5: # exclude character names and short lines
+			sys.stdout.write(line + '\n')
